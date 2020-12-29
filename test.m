6b9c02dcc4a268a31e36554cc2b8f065aab5ca46
lookforhan <31067305+lookforhan@users.noreply.github.com>
2020-12-29 21:00:06 +0800

    Create test.m
    测试文件，测试UI功能

---
+++ test.m
@@ -0,0 +1,17 @@
+% 测试生成
+a = financalApp();
+% a.UIFigure.Visible = 'off';
+% 测试datePicker
+a.DatePicker.Value
+
+% 测试UItable
+a.UITable.Data
+
+% 测试类型DropDownClass
+a.DropDownClass.Value
+
+% 测试EditFieldValue
+a.EditFieldValue.Value
+
+% 测试EditFieldRemarks
+a.EditFieldRemarks.Value
